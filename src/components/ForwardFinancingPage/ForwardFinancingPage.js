c12973d1fcd326b2ef9c9eee3f912319c6dad22e
sarareich <reichsara96@gmail.com>
2018-07-26 15:00:56 -0400

    style changes and update FF context description

--- src/components/ForwardFinancingPage/ForwardFinancingPage.js
+++ src/components/ForwardFinancingPage/ForwardFinancingPage.js
@@ -10,7 +10,9 @@ class ForwardFinancingPage extends React.Component {
     front end development and design. I had the opportunity to see many\
     projects through from start to finish, from initial wireframes to writing the final code.\
     I designed and implemented a variety of new features that matched the company’s existing\
-    UI while improving design consistency across multiple internal- and external-facing web applications."
+    UI while improving design consistency across multiple internal- and external-facing web applications.\
+    I also created design tools such as a Sketch style guide and a live documentation of\
+    the company's shared component library."
 
     return (
       <div className="forward-financing-wrapper">
@@ -20,7 +22,7 @@ class ForwardFinancingPage extends React.Component {
         <div className="forward-financing page">
           <ProjectInfo
             title="Forward Financing"
-            subtitle="UX/UI + Web Development Intern"
+            subtitle="UX/UI and Web Development Intern"
             summaryInfo={projectInfo.ffProjectInfo}
           />
           <ProjectDescription
