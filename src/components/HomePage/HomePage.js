40c2d18f68ea66cbe9d8f64e528bbe0b4a3a436a
Sara Reich <reichsara96@gmail.com>
2018-09-07 13:55:13 -0400

    rework home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,46 +4,33 @@ import FFHome from '../../assets/ForwardFinancing/FF_HomePage.png';
 import MunchHero from '../../assets/Munch/MunchHero.jpg';
 
 class HomePage extends React.Component {
+  renderImage(size, route, desiredClass, image, title, imageClass) {
+    return (
+      <NavLink
+        to={route}
+        className={`portfolio-image__container ${size} ${desiredClass}`}
+      >
+        <img
+          src={image}
+          alt={title}
+          className={`portfolio-image ${imageClass}`}
+        />
+        <div className="portfolio-image__caption">
+          <h4 className="portfolio-image__caption-text heading-3">
+            {title}
+          </h4>
+        </div>
+      </NavLink>
+    );
+  }
+
   render() {
     return (
       <div className="portfolio-home page">
         <div className="portfolio-content">
-          <NavLink
-            to="/work/forward-financing"
-            className="portfolio-image__container one-third green-gradient"
-          >
-            <img
-              src={FFHome}
-              alt="Forward Financing"
-              className="portfolio-image"
-            />
-            <div className="portfolio-image__caption">
-              <h4 className="portfolio-image__caption-text heading-3">
-                Forward Financing
-              </h4>
-            </div>
-          </NavLink>
-          <NavLink
-            to="/work/munch"
-            className="portfolio-image__container two-thirds"
-          >
-            <img
-              src={MunchHero}
-              alt="Munch"
-              className="portfolio-image non-responsive"
-            />
-            <div className="portfolio-image__caption">
-              <h4 className="portfolio-image__caption-text heading-3">
-                Munch
-              </h4>
-            </div>
-          </NavLink>
-          <div className="portfolio-image__container full-width">
-          </div>
-          <div className="portfolio-image__container two-thirds">
-          </div>
-          <div className="portfolio-image__container one-third">
-          </div>
+          {this.renderImage("full-width", "work/munch", "", MunchHero, "Munch", "non-responsive")}
+          {this.renderImage("two-thirds", "/work/forward-financing", "green-gradient", FFHome, "Forward Financing", "")}
+          {this.renderImage("one-third", "/work/miscellaneous", "", null, "Miscellaneous", "")}
         </div>
       </div>
     );
