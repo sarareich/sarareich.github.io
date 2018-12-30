f8518d44613e8cf1b163d6b7680a127d7ef6f78e
sarareich <reichsara96@gmail.com>
2018-12-30 12:17:42 -0600

    Updates

---
+++ precache-manifest.f30b3b281398d8a0bd0706c062f60730.js
@@ -0,0 +1,362 @@
+self.__precacheManifest = [
+  {
+    "revision": "317cff61a111b66b2c79f5b6952861b0",
+    "url": "/static/media/Newsfeed.317cff61.jpg"
+  },
+  {
+    "revision": "c109cc53a7e1d683f3a6",
+    "url": "/static/css/main.be7975f0.chunk.css"
+  },
+  {
+    "revision": "93f21c16fbd80993f647581d611e0a24",
+    "url": "/static/media/preferences.93f21c16.mp4"
+  },
+  {
+    "revision": "612a7294f36cff57e511",
+    "url": "/static/js/1.612a7294.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "786471d580e449e6367c183f20632c51",
+    "url": "/static/media/Resume.786471d5.pdf"
+  },
+  {
+    "revision": "d5607425583c815f66aae0fcc4b42f0c",
+    "url": "/static/media/MiscellaneousHome.d5607425.jpg"
+  },
+  {
+    "revision": "7b574ccfbf7ec6174a2f11a01508663f",
+    "url": "/static/media/FF_HomePage.7b574ccf.png"
+  },
+  {
+    "revision": "92889ffd474b3e510f64c028d659fc31",
+    "url": "/static/media/MunchHero.92889ffd.jpg"
+  },
+  {
+    "revision": "ab83a50fb5a82289846ab755256076e5",
+    "url": "/static/media/homePage.ab83a50f.png"
+  },
+  {
+    "revision": "e50e30de929bdf7588b9c534b70cb27b",
+    "url": "/static/media/tourzyHome.e50e30de.png"
+  },
+  {
+    "revision": "d8eea69e33cf1684978fbe6143d8d93c",
+    "url": "/static/media/portrait.d8eea69e.jpg"
+  },
+  {
+    "revision": "fa903f4b15df4abe3980b41092815863",
+    "url": "/static/media/MP_ideation1.fa903f4b.jpg"
+  },
+  {
+    "revision": "f9767e86add710af039bfbf78b11275a",
+    "url": "/static/media/MP_ideation2.f9767e86.jpg"
+  },
+  {
+    "revision": "8a1bfee28c863b3138530f55b0b35926",
+    "url": "/static/media/MP_ideation3.8a1bfee2.jpg"
+  },
+  {
+    "revision": "dbaf7097bfe61615c31a0f873cff6788",
+    "url": "/static/media/PP_Search_Before.dbaf7097.jpg"
+  },
+  {
+    "revision": "c7412c2c0590a33c793b27d670ac7044",
+    "url": "/static/media/PipelineSearch.c7412c2c.png"
+  },
+  {
+    "revision": "5490544083a2c4fefe6abcc20be2b620",
+    "url": "/static/media/PortfolioSearch.54905440.png"
+  },
+  {
+    "revision": "0c2a81540cdecd2483d1e5795f72f354",
+    "url": "/static/media/Home.0c2a8154.png"
+  },
+  {
+    "revision": "ba883ebe2a2aeeafca9be9450305b673",
+    "url": "/static/media/ActionsPopup.ba883ebe.png"
+  },
+  {
+    "revision": "d4ed128958efd8162f196a59482a4fbc",
+    "url": "/static/media/EditUser.d4ed1289.png"
+  },
+  {
+    "revision": "8c6848c34a7115cd2a82a23ef6eddea7",
+    "url": "/static/media/NewUser.8c6848c3.png"
+  },
+  {
+    "revision": "511b50e6df16d6fa734e18016855758f",
+    "url": "/static/media/AddUserSuccess.511b50e6.png"
+  },
+  {
+    "revision": "dadaed857b696f53c5be13fd36912c88",
+    "url": "/static/media/RemoveUserSuccess.dadaed85.png"
+  },
+  {
+    "revision": "768667183920361bd16060d4b2bd5e08",
+    "url": "/static/media/ConfirmRemoval.76866718.png"
+  },
+  {
+    "revision": "0276e0e46465cf4ba547118501c56ea0",
+    "url": "/static/media/contactinfo-closed.0276e0e4.jpg"
+  },
+  {
+    "revision": "8222d22962e5d7cdca5296533e46d537",
+    "url": "/static/media/contactinfo-open.8222d229.jpg"
+  },
+  {
+    "revision": "d9b6abb18456b1fed00477d00d3aa284",
+    "url": "/static/media/Sketch-Style-Guide.d9b6abb1.jpg"
+  },
+  {
+    "revision": "6e0c7bc25ff789e03c9fea61c0af0dce",
+    "url": "/static/media/FF_Hero.6e0c7bc2.png"
+  },
+  {
+    "revision": "77b587e2d87f78fa099532fc728ac3b2",
+    "url": "/static/media/Desktop_Statements.77b587e2.jpg"
+  },
+  {
+    "revision": "b638f03569da04f00516078354984dde",
+    "url": "/static/media/MP_Tablet_Mobile_Statements.b638f035.jpg"
+  },
+  {
+    "revision": "1e9817c56ee56a068727f2340e4d3567",
+    "url": "/static/media/Desktop_AdvancePage.1e9817c5.png"
+  },
+  {
+    "revision": "fce27d5c0f7c122cc86577ba68712cc0",
+    "url": "/static/media/Tablet_AdvancePage.fce27d5c.jpg"
+  },
+  {
+    "revision": "2a3672d50d1df0074db28c28a2710e11",
+    "url": "/static/media/Mobile_AdvancePage.2a3672d5.jpg"
+  },
+  {
+    "revision": "89948afcbeed2a348872e8ed0007c03f",
+    "url": "/static/media/Sketch-Style-Guide.89948afc.png"
+  },
+  {
+    "revision": "f16fab6563c631407464e549ee2bc8d4",
+    "url": "/static/media/MunchHome.f16fab65.jpg"
+  },
+  {
+    "revision": "fa76b83c0c4b0d019cbbd3485a207f6f",
+    "url": "/static/media/Marketing_HomePage.fa76b83c.jpg"
+  },
+  {
+    "revision": "03356870205d237995306ecb6fac44e3",
+    "url": "/static/media/Newsfeed.03356870.png"
+  },
+  {
+    "revision": "a1a47fbb401d4fb1ed33393dffc2915b",
+    "url": "/static/media/Profile.a1a47fbb.png"
+  },
+  {
+    "revision": "aeefad7acecd0fb2671c4747301749e7",
+    "url": "/static/media/RestaurantDetail.aeefad7a.png"
+  },
+  {
+    "revision": "1e46969772ac822b50544d36aeaace21",
+    "url": "/static/media/UploadPhoto.1e469697.png"
+  },
+  {
+    "revision": "65713a81d48240582d2b734a08e9f21a",
+    "url": "/static/media/Persona1.65713a81.jpg"
+  },
+  {
+    "revision": "b345b57e80ee6a59dceccb964b50fbcc",
+    "url": "/static/media/Persona2.b345b57e.jpg"
+  },
+  {
+    "revision": "1f34a23a527e389732a7743ef0a36d30",
+    "url": "/static/media/Persona3.1f34a23a.jpg"
+  },
+  {
+    "revision": "0805ff407518bf4961f8f1e28daa230f",
+    "url": "/static/media/HomePage.0805ff40.jpg"
+  },
+  {
+    "revision": "c109cc53a7e1d683f3a6",
+    "url": "/static/js/main.c109cc53.chunk.js"
+  },
+  {
+    "revision": "7a7e48cc548a1cfce37da418c1a14977",
+    "url": "/static/media/Profile.7a7e48cc.jpg"
+  },
+  {
+    "revision": "010b905f61f58295c1c27b1ceceee11f",
+    "url": "/static/media/RestaurantDetail.010b905f.jpg"
+  },
+  {
+    "revision": "23adaeb73bcb40134fc0b0f297d50a70",
+    "url": "/static/media/Search.23adaeb7.jpg"
+  },
+  {
+    "revision": "7ce1a66edaa5e0f38f9d321e256d5047",
+    "url": "/static/media/Storyboard1.7ce1a66e.jpg"
+  },
+  {
+    "revision": "00f3120bb2dc10167c2581fd125e3804",
+    "url": "/static/media/Storyboard2.00f3120b.jpg"
+  },
+  {
+    "revision": "7919cf4e431546ff4f1764771e9571ea",
+    "url": "/static/media/RestaurantSearch.7919cf4e.png"
+  },
+  {
+    "revision": "a08b0ce1f083dc301087df946ab1e203",
+    "url": "/static/media/RestaurantSearchMobile.a08b0ce1.png"
+  },
+  {
+    "revision": "a49854ae69ecaefe7f57e0f903e9516c",
+    "url": "/static/media/FriendSearch.a49854ae.png"
+  },
+  {
+    "revision": "98c59f7703f58cc19c9a68b7b905674c",
+    "url": "/static/media/FriendSearchMobile.98c59f77.png"
+  },
+  {
+    "revision": "50f16dc59350d5b2288c70fc04de99b4",
+    "url": "/static/media/PhotoUpload.50f16dc5.png"
+  },
+  {
+    "revision": "5b1576509732b28aff2877e90bfbd0fc",
+    "url": "/static/media/PhotoUploadMobile.5b157650.png"
+  },
+  {
+    "revision": "c97120f14b2e5aac0361023cab24ea2f",
+    "url": "/static/media/MobileArtboards.c97120f1.jpg"
+  },
+  {
+    "revision": "8c6fcd587c019bb045eb67434f67b09c",
+    "url": "/static/media/DesktopArtboards.8c6fcd58.jpg"
+  },
+  {
+    "revision": "47f0344204c4b7264b20808b12b97a3e",
+    "url": "/static/media/impact1.47f03442.gif"
+  },
+  {
+    "revision": "74580c7df38145411e504cf22d83989c",
+    "url": "/static/media/impact2.74580c7d.gif"
+  },
+  {
+    "revision": "2338daa5dd0233bb7fcb340a5d96797a",
+    "url": "/static/media/impact3.2338daa5.gif"
+  },
+  {
+    "revision": "79cb37bc7ac7038ea1b9ef80e3a7116d",
+    "url": "/static/media/impact4.79cb37bc.gif"
+  },
+  {
+    "revision": "f920f2e658006058f3c5fc4b235e68ff",
+    "url": "/static/media/cookieCatcher1.f920f2e6.gif"
+  },
+  {
+    "revision": "2d17048a763941c80c70c6a9c93f080c",
+    "url": "/static/media/cookieCatcher2.2d17048a.gif"
+  },
+  {
+    "revision": "b16b38797d780663403e6bc699ec64a1",
+    "url": "/static/media/cookieCatcher3.b16b3879.gif"
+  },
+  {
+    "revision": "3ef63b122f7b67a6dd30bd3cdd50fd28",
+    "url": "/static/media/quoteGenerator.3ef63b12.gif"
+  },
+  {
+    "revision": "fa07ddc31b04ece8f0e7139330c5b97e",
+    "url": "/static/media/weather.fa07ddc3.gif"
+  },
+  {
+    "revision": "eb188c04816315aab25a4877242fd325",
+    "url": "/static/media/InteractionFlow.eb188c04.jpg"
+  },
+  {
+    "revision": "03ad9afc9d0d083b9d79c30f72bfc598",
+    "url": "/static/media/UISpec.03ad9afc.png"
+  },
+  {
+    "revision": "9da907e480fda731950d03552eb2f406",
+    "url": "/static/media/PaperPrototype.9da907e4.jpg"
+  },
+  {
+    "revision": "089e4aa41bd3f06dda2ece24362fea6f",
+    "url": "/static/media/doughScreens1.089e4aa4.png"
+  },
+  {
+    "revision": "e57044c3aeba847ea915e982f336e449",
+    "url": "/static/media/doughScreens2.e57044c3.png"
+  },
+  {
+    "revision": "6f5e2076ebc0eb52a847ea4855b13968",
+    "url": "/static/media/doughScreens3.6f5e2076.png"
+  },
+  {
+    "revision": "b02e7d09b4023ef0f2b936edc049a808",
+    "url": "/static/media/doughScreens4.b02e7d09.png"
+  },
+  {
+    "revision": "0da0d80ac6538b309290ba8192263a5d",
+    "url": "/static/media/hero.0da0d80a.jpg"
+  },
+  {
+    "revision": "79a41736d4dc330f18b8d813728ad233",
+    "url": "/static/media/taskAnalysis.79a41736.png"
+  },
+  {
+    "revision": "59b0cb37a9e5cd753dbbb486561739cd",
+    "url": "/static/media/brainstorming.59b0cb37.JPG"
+  },
+  {
+    "revision": "0f9e89ae5e25b5f38e75f918ca1d7fbc",
+    "url": "/static/media/heroJourney.0f9e89ae.jpg"
+  },
+  {
+    "revision": "72146b4b3a9884dd62855313090c1572",
+    "url": "/static/media/storyboard.72146b4b.jpg"
+  },
+  {
+    "revision": "bf204c46baac7a1042cbf8e2cdf15020",
+    "url": "/static/media/UISpec.bf204c46.png"
+  },
+  {
+    "revision": "263eb9e6018ad0a4c2f9c00602c4f0a7",
+    "url": "/static/media/tourzyHero.263eb9e6.jpg"
+  },
+  {
+    "revision": "4aebfaa950f18c657b4a175b52a1f2b1",
+    "url": "/static/media/UISpec.4aebfaa9.pdf"
+  },
+  {
+    "revision": "e09092a28a50261bc052889d5c7f11a7",
+    "url": "/static/media/artboards.e09092a2.pdf"
+  },
+  {
+    "revision": "6f9e03fcdbce1114187a6ab09c545886",
+    "url": "/static/media/alert.6f9e03fc.mp4"
+  },
+  {
+    "revision": "26ddb1db9dfc844fefd214ec2ea8da1a",
+    "url": "/static/media/editTrip.26ddb1db.mp4"
+  },
+  {
+    "revision": "1f4e2048a0c6d463dbffe7a72d9412ab",
+    "url": "/static/media/itinerary.1f4e2048.mp4"
+  },
+  {
+    "revision": "dc0f2229f53efccb761959099a13f4b3",
+    "url": "/static/media/newTrip.dc0f2229.mp4"
+  },
+  {
+    "revision": "612a7294f36cff57e511",
+    "url": "/static/css/1.fb83e16c.chunk.css"
+  },
+  {
+    "revision": "275a1161e32a74d5681a2f87df7d10f3",
+    "url": "/index.html"
+  }
+];
