1f1755c63703e87e0016989c0f230463ce1e47d1
sarareich <reichsara96@gmail.com>
2019-05-29 10:18:12 -0400

    research and ideation

--- src/components/MindfullyPage/MindfullyPage.js
+++ src/components/MindfullyPage/MindfullyPage.js
@@ -3,7 +3,8 @@ import VisibilitySensor from 'react-visibility-sensor';
 import ScrollUp from '../common/ScrollUp/ScrollUp';
 import LightBox from '../common/Lightbox/Lightbox';
 import Video from '../common/Video/Video';
-import tourzyImages from '../../constants/tourzyImages';
+import mindfullyImages from '../../constants/mindfullyImages';
+import { Carousel } from 'react-responsive-carousel';
 import Hero from '../common/Hero/Hero';
 import ProjectInfo from '../common/ProjectInfo/ProjectInfo';
 import Title from '../common/ProjectAssets/Title';
@@ -12,6 +13,76 @@ import projectInfo from '../../constants/projectInfo';
 import MindfullyHero from '../../assets/Mindfully/hero.jpg';
 
 class MindfullyPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      researchLightboxIsOpen: false
+    };
+
+    this.openLightbox = this.openLightbox.bind(this);
+  }
+
+  openLightbox(index, event, lightbox) {
+    event.preventDefault();
+    this.setState({
+      [lightbox]: true,
+      photoIndex: index
+    });
+  }
+
+  closeLightbox(lightbox) {
+    this.setState({
+      [lightbox]: false,
+      photoIndex: 0
+    });
+  }
+
+  nextLightboxImage(images) {
+    this.setState({
+      photoIndex: (this.state.photoIndex + 1) % images.length,
+    });
+  }
+
+  prevLightboxImage(images) {
+    this.setState({
+      photoIndex: (this.state.photoIndex + images.length - 1) % images.length,
+    });
+  }
+
+  renderImageRow(image, index, altText, lightbox, desiredClass, imageClass) {
+    return (
+      <div className={desiredClass} key={index}>
+        <img
+          src={image}
+          className={`project-image__small zoom ${imageClass}`}
+          alt={altText}
+          onClick={(event) => this.openLightbox(index, event, lightbox)}
+        />
+      </div>
+    );
+  }
+
+  renderLightbox(lightboxImages, lightbox, lightboxString, index) {
+    return (
+      <LightBox
+        images={lightboxImages}
+        isOpen={lightbox}
+        onClose={this.closeLightbox.bind(this, lightboxString)}
+        nextImage={this.nextLightboxImage.bind(this, lightboxImages)}
+        prevImage={this.prevLightboxImage.bind(this, lightboxImages)}
+        photoIndex={index}
+      />
+    );
+  }
+
+  renderCarouselImages(image, index, altText) {
+    return (
+      <div key={index}>
+        <img src={image} alt={altText}/>
+      </div>
+    );
+  }
+
   renderHero() {
     return (
       <Hero
@@ -76,10 +147,109 @@ class MindfullyPage extends React.Component {
               Mindfully seeks to address the <strong>deficit of emotional literacy</strong> during childhood
               through an app that provides a supplemental, individual classroom experience. The app
               teaches children to identify and define their emotions, and covers strategies to manage
-              those emotions and reduce emotional distress. Ultimately, this project seeks to enable students
-              to be more successful as individuals by <strong>making complex information about emotional literacy
-              accessible and fun for children.</strong>
+              those emotions. This project seeks to enable students to be more successful as individuals by
+              <strong> making complex information about emotional literacy accessible and fun.</strong>
             </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderResearch() {
+    const { photoIndex, researchLightboxIsOpen } = this.state;
+    return (
+      <div className="user-research">
+        <div className="portfolio-content description">
+          <Title
+            title="Research"
+          />
+          <div className="two-thirds text">
+            <p className="description-text">
+              My team conducted expert interviews and secondary research to learn more about the issues faced
+              by low-income elementary school students. We interviewed teachers and mental health professionals,
+              and read literature on topics ranging from parent participation in their children's education to
+              the impact of mindfulness in the classroom.
+            </p>
+          </div>
+          <div className="two-thirds__center shadow">
+            <Carousel dynamicHeight={false}>
+              {mindfullyImages.research.map((image, index) => this.renderCarouselImages(image, index, "Research"))}
+            </Carousel>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderIdeation() {
+    const { photoIndex, researchLightboxIsOpen } = this.state;
+    return (
+      <div className="user-research">
+        <div className="portfolio-content description">
+          <Title
+            title="Ideation"
+          />
+          <div className="two-thirds text">
+            <p className="description-text">
+              Based on our research, we came up with ideas for three areas where we saw a need for intervention in
+              low income elementary schools: <strong>parent involvement, curriculum buildling, and
+              mental health. </strong> We created simple paper prototypes for each of these ideas.
+            </p>
+          </div>
+        </div>
+        <div className="portfolio-content description">
+          <div className="one-third text">
+            <Subtitle
+              subtitle="Parent Involvement"
+            />
+          </div>
+          <div className="two-thirds text">
+            <p className="description-text">
+              A network for parents to communicate with each other and get involved in school events.
+            </p>
+            <div className="shadow">
+              <Carousel dynamicHeight={false}>
+                {mindfullyImages.parentNetwork.map((image, index) => this.renderCarouselImages(image, index, "Parent Network"))}
+              </Carousel>
+            </div>
+          </div>
+        </div>
+
+        <div className="portfolio-content description">
+          <div className="one-third text">
+            <Subtitle
+              subtitle="Curriculum Buildling"
+            />
+          </div>
+          <div className="two-thirds text">
+            <p className="description-text">
+              A tool for teachers to create curriculums based on grade level and state standards, and get advice
+              from other teachers.
+            </p>
+            <div className="shadow">
+              <Carousel dynamicHeight={false}>
+                {mindfullyImages.curriculum.map((image, index) => this.renderCarouselImages(image, index, "Curriculum Builder"))}
+              </Carousel>
+            </div>
+          </div>
+        </div>
+
+        <div className="portfolio-content description">
+          <div className="one-third text">
+            <Subtitle
+              subtitle="Mental Health"
+            />
+          </div>
+          <div className="two-thirds text">
+            <p className="description-text">
+              A tablet app that encourages students to meditate and reflect on their feelings.
+            </p>
+            <div className="shadow">
+              <Carousel dynamicHeight={false}>
+                {mindfullyImages.meditation.map((image, index) => this.renderCarouselImages(image, index, "Meditation"))}
+              </Carousel>
+            </div>
           </div>
         </div>
       </div>
@@ -94,6 +264,8 @@ class MindfullyPage extends React.Component {
           <ScrollUp />
           {this.renderProjectInfo()}
           {this.renderBackground()}
+          {this.renderResearch()}
+          {this.renderIdeation()}
         </div>
       </div>
     );
---
+++ src/constants/mindfullyImages.js
@@ -0,0 +1,25 @@
+import ResearchQuestions from '../assets/Mindfully/ResearchQuestions.png';
+import Interviews from '../assets/Mindfully/Interviews.png';
+import SecondaryResearch from '../assets/Mindfully/SecondaryResearch.png';
+import Network1 from '../assets/Mindfully/ParentNetwork/Network1.jpg';
+import Network2 from '../assets/Mindfully/ParentNetwork/Network2.jpg';
+import Network3 from '../assets/Mindfully/ParentNetwork/Network3.jpg';
+import Network4 from '../assets/Mindfully/ParentNetwork/Network4.jpg';
+import Network5 from '../assets/Mindfully/ParentNetwork/Network5.jpg';
+import Curriculum1 from '../assets/Mindfully/Curriculum/Curriculum1.jpg';
+import Curriculum2 from '../assets/Mindfully/Curriculum/Curriculum2.jpg';
+import Curriculum3 from '../assets/Mindfully/Curriculum/Curriculum3.jpg';
+import Curriculum4 from '../assets/Mindfully/Curriculum/Curriculum4.jpg';
+import Meditation1 from '../assets/Mindfully/Meditation/Meditation1.jpg';
+import Meditation2 from '../assets/Mindfully/Meditation/Meditation2.jpg';
+import Meditation3 from '../assets/Mindfully/Meditation/Meditation3.jpg';
+import Meditation4 from '../assets/Mindfully/Meditation/Meditation4.jpg';
+import Meditation5 from '../assets/Mindfully/Meditation/Meditation5.jpg';
+import Meditation6 from '../assets/Mindfully/Meditation/Meditation6.jpg';
+
+export default {
+  research: [ResearchQuestions, Interviews, SecondaryResearch],
+  parentNetwork: [Network1, Network2, Network3, Network4, Network5],
+  curriculum: [Curriculum1, Curriculum2, Curriculum3, Curriculum4],
+  meditation: [Meditation1, Meditation2, Meditation3, Meditation4, Meditation5, Meditation6]
+}
